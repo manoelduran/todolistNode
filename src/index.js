1a74ba0e19cf06ca7dc10a4b89d7c089c3c4538b
Manoel Duran <manoel.duran@hotmail.com>
2022-06-18 15:10:55 -0300

    post user

--- src/index.js
+++ src/index.js
@@ -1,25 +1,46 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const user = users.find(user => user.username === username);
+  if (!user) {
+    return response.status(400).json({ error: "user not exist!" })
+  }
+  request.user = user;
+  return next();
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const {name, username} = request.body; 
+  const userALreadyExists = users.some(
+    (user) => user.username === username
+);
+  if (userALreadyExists) {
+    return response.status(404).json({ error: "User already exists!" });
+};
+users.push({
+  id: uuidv4(),
+  name,
+  username,
+  todos: []
+});
+return response.status(201).send(users);
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user} = request;
+  
+
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
